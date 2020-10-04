f06bef712afe31b742969961ad8c61b89186332b
Anton Strogonoff <anton@strogonoff.name>
2020-10-05 00:56:59 +0900

    cleanup(schema): drop commented-out logic

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,9 +1,7 @@
 import { MarkSpec, NodeSpec, Schema } from 'prosemirror-model';
-//import OrderedMap from 'orderedmap';
 
 
 export interface SchemaFeature<N extends string = any, M extends string = any> {
-  //schema: Schema<N, M>
   nodes?: Record<N, NodeSpec>
   marks?: Record<M, MarkSpec>
 }
@@ -12,27 +10,6 @@ export interface SchemaFeature<N extends string = any, M extends string = any> {
 export default function featuresToSchema<K extends SchemaFeature[]>
 (features: K):
 Schema<any, any> {
-
-  //const schema = features.map(f => f.schema).reduce((prev, curr) => {
-
-  //  const nodes = OrderedMap.from<NodeSpec>({
-  //    ...prev.spec.nodes,
-  //    ...curr.spec.nodes,
-  //  } as { [key: string]: NodeSpec });
-
-  //  const marks = OrderedMap.from<MarkSpec>({
-  //    ...prev.spec.marks,
-  //    ...curr.spec.marks,
-  //  } as { [key: string]: MarkSpec });
-
-  //  return new Schema({ nodes, marks });
-
-  //}, new Schema({ nodes: { doc: { content: "block+" } } }));
-
-  //debugger;
-  //console.info("GOT SCHEMA", schema);
-
-  //return schema;
 
   const nodes = features.
     map(f => f.nodes || {}).
